50f453e32a6ac017a7cd228e3e086a4412e840f7
ha4219 <ha4219@naver.com>
2023-04-17 21:37:59 +0900

    fix err

--- src/utils/card/opgg.ts
+++ src/utils/card/opgg.ts
@@ -118,7 +118,7 @@ export function opggCard({
 }
 
 export function opggNullCard() {
-  const imgSrc = getTierImg({ tier: null });
+  const imgSrc = getTierImg(null);
 
   return `<svg
     xmlns="http://www.w3.org/2000/svg"
