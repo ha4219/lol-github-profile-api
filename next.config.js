96f7ae8eea064543e2e97f354af1a54b42f7fde7
ha4219 <ha4219@naver.com>
2023-04-17 21:04:13 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['lol-github-profile-api.vercel.app'],
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src lol-github-profile-api.vercel.app;
+  style-src 'self' lol-github-profile-api.vercel.app;
+  img-src 'self' lol-github-profile-api.vercel.app
+  font-src 'self';  
+`;
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['lol-github-profile-api.vercel.app'],
+//   },
+// };
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/assets',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
   },
 };
-
-module.exports = nextConfig;
