f79f75b3cb501936e7077210387feb677362bb8c
ha4219 <ha4219@naver.com>
2023-04-24 22:10:32 +0900

    test

---
+++ src/pages/api/test2.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { SummaryBox1 } from '@/utils/card/opgg';
+import { renderToString } from 'react-dom/server';
+import { renderStylesToString } from '@emotion/server';
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.setHeader(
+    'Cache-Control',
+    'public, immutable, no-transform, max-age=3600',
+  );
+  const d = renderStylesToString(renderToString(SummaryBox1()));
+  return res.end(d);
+}
--- src/utils/card/cards.ts
+++ src/utils/card/cards.ts
@@ -1,9 +1,11 @@
-import { OpggCard, OpggNullCard } from './opgg';
+import { OpggCard, OpggNullCard, SummaryBox, SummaryBox1 } from './opgg';
 import { SovledDotAcCard, SovledDotAcNullCard } from './solvedDotAc';
 
 const cards = [
   { Main: OpggCard, Null: OpggNullCard },
   { Main: SovledDotAcCard, Null: SovledDotAcNullCard },
+  { Main: SummaryBox, Null: SovledDotAcNullCard },
+  { Main: SummaryBox1, Null: SovledDotAcNullCard },
 ];
 
 export default cards;
